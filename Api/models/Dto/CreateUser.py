d94fecfdc3e50ac826b3e1ac613ecb7e09347fc2
Akshay Raut <87926985+meakshayraut@users.noreply.github.com>
2023-11-20 11:22:59 +0530

    Update and rename CreateUser.py.txt to CreateUser.py
    Added Dto/CreateUser.py

---
+++ Api/models/Dto/CreateUser.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from pydantic import BaseModel,Field
+ 
+ 
+# User models
+ 
+class CreateUser(BaseModel):
+    name: str = Field(min_length=1, max_length=16)
+    profession: str = Field(min_length=1, max_length=16)
+    phone : str
+    imageUrl: str
+    created_at : str
+    updated_at : str
+    isActive: Optional[bool] = False
+ 
